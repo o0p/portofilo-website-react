22bf2eaac7b1a99e4ae9d978f43df25d3a85a35a
Ruslan <rus@gmx.com>
2021-05-09 19:22:03 +0100

    Add fetchJobs()

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  const [loading, setloading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
   const [value, seValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const newJobs = await res.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
 
   return <h2>tabs project setup</h2>;
 }
