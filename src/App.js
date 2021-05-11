f4c508d4b7cd7248cb9f8b1e1fbd63b6483e1f1c
Ruslan <rus@gmx.com>
2021-05-11 23:41:54 +0100

    Add btns

--- src/App.js
+++ src/App.js
@@ -36,6 +36,11 @@ function App() {
       </div>
       <div className="jobs-center">
         {/* {btn} */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return <button>{item.company}</button>;
+          })}
+        </div>
         {/* {job info} */}
         <article className="job-info">
           <h3>{title}</h3>
